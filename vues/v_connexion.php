afd01221f16c96cede504fd59981c8f36718971a
Jacksly91 <jack.okissi@gmail.com>
2014-04-24 10:01:27 +0200

    Mise à jour v_connexion.php

--- vues/v_connexion.php
+++ vues/v_connexion.php
@@ -4,18 +4,20 @@
 
 <form method="POST" action="index.php?uc=connexion&action=valideConnexion">
    
-    
-			<p>
-       <label for="login">Login*</label>
-       <input id="login" type="text" name="login"  size="30" maxlength="45">
-      </p>
-			<p>
-				<label for="mdp">Mot de passe*</label>
-			  <input id="mdp"  type="password"  name="mdp" size="30" maxlength="45">
-      </p>
-         <input type="submit" value="Valider" name="valider">
-         <input type="reset" value="Annuler" name="annuler"> 
-      </p>
+	<p>
+        	<label for="login">Login*</label>
+        	<input id="login" type="text" name="login"  size="30" maxlength="45">
+	</p>
+       
+	<p>
+	 	<label for="mdp">Mot de passe*</label>
+		<input id="mdp"  type="password"  name="mdp" size="30" maxlength="45">
+	</p>
+	
+	<p>
+         	<input type="submit" value="Valider" name="valider">
+         	<input type="reset" value="Annuler" name="annuler"> 
+	</p>
 </form>
 
 </div>
